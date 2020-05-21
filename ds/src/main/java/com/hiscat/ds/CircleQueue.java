7102104897e126a982ba037ee6622f35502d57df
1251723871@qq.com <smailXie@163.com>
2020-05-22 05:58:18 +0800

    feat(ds):add CircleQueue

---
+++ ds/src/main/java/com/hiscat/ds/CircleQueue.java
@@ -0,0 +1,64 @@
+package com.hiscat.ds;
+
+/**
+ * @author hiscat
+ */
+public class CircleQueue {
+    private int head;
+    private int tail;
+    private int maxSize;
+    private int[] elements;
+    private int size;
+
+    public CircleQueue(int maxSize) {
+        this.maxSize = maxSize;
+        elements = new int[maxSize];
+        head = tail = 0;
+        size = 0;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public boolean isFull() {
+        return size == maxSize;
+    }
+
+    public boolean put(int i) {
+        if (isFull()) {
+            return false;
+        }
+        tail = (tail + 1) % maxSize;
+        elements[tail] = i;
+        size++;
+        return true;
+    }
+
+    public int take() {
+        if (isEmpty()) {
+            throw new NullPointerException("queue is empty!");
+        }
+        size--;
+        head = (head + 1) % maxSize;
+        return elements[head];
+    }
+
+    public int peek() {
+        return elements[(head + 1) % maxSize];
+    }
+
+    public void show() {
+        if (isEmpty()) {
+            System.out.println("queue is empty!");
+            return;
+        }
+        for (int index = head; index < head + size; index++) {
+            System.out.printf("arr[%d]=%d", index % maxSize, elements[index % maxSize]);
+        }
+    }
+
+    public int size() {
+        return this.size;
+    }
+}
---
+++ ds/src/test/java/com/hiscat/ds/CircleQueueTest.java
@@ -0,0 +1,66 @@
+package com.hiscat.ds;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CircleQueueTest {
+
+    @Test
+    void isEmpty() {
+        assertTrue(new CircleQueue(1).isEmpty());
+    }
+
+    @Test
+    void isFull() {
+        CircleQueue circleQueue = new CircleQueue(1);
+        assertFalse(circleQueue.isFull());
+        circleQueue.put(1);
+        assertTrue(circleQueue.isFull());
+    }
+
+    @Test
+    void put() {
+        assertTrue(new CircleQueue(1).put(1));
+    }
+
+    @Test
+    void take() {
+        CircleQueue circleQueue = new CircleQueue(2);
+        assertTrue(circleQueue.put(1));
+        assertTrue(circleQueue.put(2));
+        assertEquals(1, circleQueue.take());
+        assertTrue(circleQueue.put(1));
+        assertEquals(2, circleQueue.take());
+        assertEquals(1, circleQueue.take());
+    }
+
+    @Test
+    void peek() {
+        CircleQueue circleQueue = new CircleQueue(2);
+        assertTrue(circleQueue.put(1));
+        assertTrue(circleQueue.put(2));
+        assertEquals(1, circleQueue.peek());
+        assertEquals(1, circleQueue.take());
+        assertEquals(2, circleQueue.take());
+        assertTrue(circleQueue.put(1));
+        assertEquals(1, circleQueue.peek());
+    }
+
+    @Test
+    void show() {
+        CircleQueue circleQueue = new CircleQueue(2);
+        circleQueue.put(1);
+        circleQueue.put(2);
+        circleQueue.show();
+    }
+
+    @Test
+    void size() {
+        CircleQueue circleQueue = new CircleQueue(2);
+        circleQueue.put(1);
+        assertEquals(1, circleQueue.size());
+        circleQueue.put(2);
+        assertEquals(2, circleQueue.size());
+    }
+}
